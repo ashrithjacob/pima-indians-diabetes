2ee748427ca9a67a190bbff8246a25b562260a21
ashrithjacob <ashrithjacob@gmail.com>
2022-11-13 23:15:38 +0100

    adding manual precision and black linter

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ Created on Saturday Nov 5 10:36:40 2022
 
 DATASET: pima-indians-diabetes dataset
 Information on dataset: SEE README
+
+'black' package used as linter
 """
 
 import sys
-import os 
+import os
 import numpy as np
 import pandas as pd
 from data import *
@@ -18,22 +20,22 @@ from sklearn.metrics import precision_score
 """
 Editable parameters by user:
 """
-PATH = 'dataset/pima-indians-diabetes-withcol.csv' # path to dataset
-test_split = 0.2 # what fraction of total data is to be used as test split
+PATH = "dataset/pima-indians-diabetes-withcol.csv"  # path to dataset
+test_split = 0.2  # what fraction of total data is to be used as test split
 missing_val = np.NaN
 number_of_trees = 10
 """
 End of editable section
 """
 
-model_file = 'pima_'+str(number_of_trees)+'.model'
+model_file = "pima_" + str(number_of_trees) + ".model"
 
 # read csv
 df = pd.read_csv(PATH)
 
 # Set dependant and independant variables
 ncols = df.shape[1]
-X = df.iloc[:, 0:ncols - 1]
+X = df.iloc[:, 0 : ncols - 1]
 Y = df.iloc[:, ncols - 1]
 
 # clean data
@@ -43,46 +45,67 @@ clean(df, missing_val)
 display(df, ncols, X, Y)
 
 # split into train and test
-X_train, X_test, Y_train, Y_test, scale_pos_weight = \
-    split(X, Y, test_split)
-#X_train and X_test - contain 8 columns + index column
-#Y_train and Y_test - contain 1 column + index column
+X_train, X_test, Y_train, Y_test, scale_pos_weight = split(X, Y, test_split)
+# X_train and X_test - contain 8 columns + index column
+# Y_train and Y_test - contain 1 column + index column
 
 # main only execs if this is the run as script
-if __name__ == '__main__' :
-    if (os.path.exists('./'+str(model_file))):
+if __name__ == "__main__":
+    if os.path.exists("./" + str(model_file)):
         flag = 1
-        print ("Model file exists, skipping model generation...")
+        print("Model file exists, skipping model generation...")
     else:
         flag = 0
 
-if (flag != 1):
-    boosted_tree = \
-        generate_model(
-            X_train,
-            X_test,
-            Y_train,
-            Y_test,
-            scale_pos_weight,
-            missing_val,
-            model_file,
-            number_of_trees)
+if flag != 1:
+    boosted_tree = generate_model(
+        X_train,
+        X_test,
+        Y_train,
+        Y_test,
+        scale_pos_weight,
+        missing_val,
+        model_file,
+        number_of_trees,
+    )
 
+# Y_pred is a series object
 Y_pred = predict_test(X_test, model_file, missing_val)
-# Y_pred is a series
 
-print ("X_test shape \n", X_test.size)
-print ("X_test \n", X_test)
-print ("Y_pred shape", Y_pred.size)
-print ("Y_test shape", Y_test.size) 
-print ("getting first columns of Y_test \n",Y_test.head()) 
-Y_test2 = Y_test.sort_index()
-print ( "sorted by index \n",Y_test2)
-print ("after sorting: \n", Y_test2.head())
+print("X_test shape \n", X_test.size)
+print("Y_pred shape \n", Y_pred.size)
+print("Y_test shape \n", Y_test.size)
+print("getting first columns of Y_test \n", Y_test.head())
+# Y_test2 = Y_test.sort_index()
+# print("sorted by index \n", Y_test2)
+# print("after sorting: \n", Y_test2.head())
 best_preds = np.rint(Y_pred)
-print("Y_pred", Y_pred)
-print("best pred", best_preds)
-print ("Numpy array precision:", precision_score(Y_test, best_preds, average='macro'))
+print(
+    "Predicted float Y values and best Y predicted values \n",
+    Y_pred,
+    "\t",
+    best_preds,
+)
 
-# TODO print the above dataframes side by side
-# TODO sort local and origin repo iloc(axis=0))- done (object is a series)
+ps = precision_score(Y_test, best_preds, average="binary")
+len = Y_pred.size
+tp = 0
+fp = 0
+for i in range(len):
+    tp = (
+        tp + 1
+        if Y_test.iloc[i : i + 1].values == best_preds[i]
+        and Y_test.iloc[i : i + 1].values == 1
+        else tp
+    )
+    fp = (
+        fp + 1
+        if Y_test.iloc[i : i + 1].values != best_preds[i]
+        and Y_test.iloc[i : i + 1].values == 0
+        else fp
+    )
+    print("true or false", Y_test.iloc[i : i + 1].values == 0)
+    # best_preds[i]
+
+print("Numpy array precision:", ps)
+print("manually calculated precison", tp / (tp + fp))
